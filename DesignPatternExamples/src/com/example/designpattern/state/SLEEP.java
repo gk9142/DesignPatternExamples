eb627b8d88bf2c48c8c0c1405d6141fefc0d85e5
gk9142 <66246693+gk9142@users.noreply.github.com>
2020-06-01 20:32:46 +0900

    Update SLEEP.java

--- DesignPatternExamples/src/com/example/designpattern/state/SLEEP.java
+++ DesignPatternExamples/src/com/example/designpattern/state/SLEEP.java
@@ -1,10 +1,11 @@
 package com.example.designpattern.state;
 
-calss SLEEP implements Stata {
+calss SLEEP implements State {
  public void on_button(Light light){
    light.setState( new ON()  );
    }
+ 
  public void off_button(Light light){
    light.setState( new OFF()  );
    } 
-   
+}   
